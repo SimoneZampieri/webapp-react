b8442e75922d24e72132eb5c732a76c355fe57c3
SimoneZampieri <simonezampieri106@gmail.com>
2025-02-07 14:20:38 +0100

    [update] add GlobalContext for movie fetching

---
+++ src/context/GlobalContext.jsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { useState, useContext, createContext } from "react";
+
+const GlobalContext = createContext();
+
+const GlobalProvider = ({ children }) => {
+  const apiUrl = "http://localhost:3000";
+
+  const fetchMovies = () => {
+    axios.get(apiurl).then((res) => {
+      console.log(res.data);
+    });
+  };
+
+  const value = {
+    fetchMovies,
+  };
+
+  return (
+    <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>
+  );
+};
+
+const useGlobalContext = () => {
+  return useContext(GlobalContext);
+};
+
+export { GlobalProvider, useGlobalContext };

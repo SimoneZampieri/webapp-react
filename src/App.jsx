b825475454418b7f1d6fff559c68510c6dcaeebc
SimoneZampieri <simonezampieri106@gmail.com>
2025-02-07 14:12:36 +0100

    [update] Implement routing with Home and DefLayout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import DefLayout from "./components/DefLayout";
 
+//app function
 function App() {
-  return <>APP</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DefLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/DefLayout.jsx
+++ src/components/DefLayout.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Header";
 
 const DefLayout = () => {
   return (
-    <div className="container mt-3">
-      <Outlet />
+    <div>
+      <Header />
+      <div className="container mt-3">
+        <Outlet />
+      </div>
     </div>
   );
 };

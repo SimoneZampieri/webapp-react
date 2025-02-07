33f667ec1ffa4a468b5cd452e2cda9a0d9b1b0c5
SimoneZampieri <simonezampieri106@gmail.com>
2025-02-07 16:05:38 +0100

    [update] Add image display to MovieCard component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const MovieCard = ({ movie }) => {
   return (
     <div className="card mb-3">
+      <img src={movie.image_url} className="card-img-top" />
       <div className="card-body">
         <h5 className="card-title">{movie.title}</h5>
         <p className="card-text">{movie.abstract}</p>
